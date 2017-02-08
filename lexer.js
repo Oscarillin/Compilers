bf770c4cda9e6a1c5cd676476cb13cff2d42ebe9
Oscarillin <oscarillin1@gmail.com>
2017-02-08 01:35:25 -0500

    Added a way to separate tokens when presented with a seperator.
    -New function separatewords() sends the accept states to an array that will keep track of all accepted states to convert to tokens
    -New function listState() will also take the returned value of findState() and pushed that into the array as well.
    -Pulled out a few hairs because i had done assignment somewhere, when i should have been checking for equality.

--- lexer.js
+++ lexer.js
@@ -3,10 +3,10 @@
 
 
 
-
-
-
-var symArray =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','(',')','{','}','=','+','!','"']
+var returnedArray = []
+var isSeperator = [" ","(",")","{","}","=","+"];
+var acceptedState = [1,5,6,10,11,12,14,15,20,21,27,28,32,33,36,37,38,39,40,41,42,43,44,46,47]
+var symArray =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','(',')','{','}','=','+','!','"'," "]
 var lexArray = [[37 ,21 ,37 ,37 ,37 ,28 ,37 ,37 ,11 ,37 ,37 ,37 ,37 ,37 ,37 , 1 ,37 ,37 ,15 ,33 ,37 ,37 , 6 ,37 ,37 ,37 ,43 ,43 ,43 ,43 ,43 ,43 ,43 ,43 ,43 ,43 ,38 ,39 ,40 ,41 ,42 ,46 , 45, 48],
 			 	[-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 , 2 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 , -1, -1],
 			 	[-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 , 3 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 , -1, -1],
@@ -56,63 +56,78 @@ var lexArray = [[37 ,21 ,37 ,37 ,37 ,28 ,37 ,37 ,11 ,37 ,37 ,37 ,37 ,37 ,37 , 1 
 			 	[-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 , -1, -1],
 			 	[-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 , -1, -1]];
 
-var currentState = 0;
-var nextState;
+var currentState = 0; // States that the begin state is 0
 
-//CurrentPos will tell the position where at when reading the string input
-var currentPos = 0;
-//Buffer will take the string inputed. 
-var buffer = "print";
-
-// function checkInput(){
-// 	for(var i = 0; i < buffer.length; ++i ){
-// 		if(symArray.includes(buffer.charAt(currentPos))){
-// 			return symArray.indexOf(buffer);
-// 		} else {
-// 			//error
-// 		}
-// 	}
-// }
-// console.log(checkInput());
-
-
+var currentPos = 0; //CurrentPos will tell the position we're at when reading the string input
+var buffer = "print(while"; //Buffer will take the string inputed.
 
 function findState(){
-	while(nextState != -1){
+	while(currentPos < buffer.length){
 		var currentChar = buffer.charAt(currentPos);
-		console.log(currentChar);
 		var nextChar = buffer.charAt(currentPos + 1);
-		console.log(nextChar);
+		console.log(currentChar)
 		if (symArray.includes(currentChar)){
-			var pointer = symArray.indexOf(currentChar);
-			currentPos++;
-			console.log(currentPos);
-			currentChar = nextChar;
-			console.log(currentChar);
+			var pointer = symArray.indexOf(currentChar);	
 		} else {
-		//Not in our language. Throw error
+		   console.log("Invalid Input Received");
+		   return;
 		}	
-	nextState = lexArray[currentState][pointer];
-	console.log(nextState);
-		if(nextState != -1){
-			currentState = nextState;
-			console.log(currentState);
-		}
+		currentState = lexArray[currentState][pointer];
+		currentPos++;
+		if(isSeperator.includes(nextChar)){
+			seperateWords();
+			console.log("seperator here");
+		} 
+		if(isSeperator.includes(currentChar)){
+		 	seperateWords();
+		 	console.log("seperator here");
+		 }
+		currentChar = nextChar;
 	} return currentState;
 }
-
 console.log(findState());
+console.log(returnedArray);
+
+function seperateWords(){
+	console.log(currentState);
+	if (currentState == -1){
+		console.log("Potato");
+	} else {
+		if(acceptedState.includes(currentState)){
+			returnedArray.push(currentState);
+		} else {
+			console.log("Tomato")
+		}
+	}
+	currentState = 0;
+}
+
+function listState(){
+	var returnedState = findState();
+	console.log(returnedState);
+	if (acceptedState.includes(returnedState)){
+		returnedArray.push(returnedState);
+		console.log(returnedArray);
+	}
+}
+console.log(listState());
+console.log(returnedArray);
 
 
-
-var acceptedState = [1,5,6,10,11,12,14,15,20,21,27,28,32,33,36,37,38,39,40,41,42,43,44,45,46]
-
-
-
-
-
-
-
-
-
-
+// function isSepertor(nextChar){
+// 	if (nextChar.charAt(currentPos) == " "){
+// 		return true;
+// 	} else if (nextChar.charAt(currentPos) == "="){
+// 		return true;
+// 	} else if (nextChar.charAt(currentPos) == "("){
+// 		return true;
+// 	} else if (nextChar.charAt(currentPos) == ")"){
+// 		return true;
+// 	} else if (nextChar.charAt(currentPos) == "{"){
+// 		return true;
+// 	} else if (nextChar .charAt(currentPos) == "}"){
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }

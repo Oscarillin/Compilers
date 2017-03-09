234a08bf1fdea3df5fc71d1f2ed013b849a3b4df
Oscarillin <oscarillin1@gmail.com>
2017-03-08 20:17:43 -0500

    Started basic functionality on parser
    -Basic outline of how code should run

---
+++ parser.js
@@ -0,0 +1,129 @@
+//Oscar Reyes
+//Parser
+
+var currentIndex = 0;
+var tokenList
+var nextToken;
+
+function parseProgram(){
+	parseBlock();
+	//match "EOP"
+}
+
+function parseBlock(){
+	//match LBRACE
+	parseStatementList();
+	//match RBRACE
+}
+
+function parseStatementList(){
+	if(nextToken != "RBRACE"){
+		parseStatement();
+		parseStatementList();
+	} else {
+		//Epsilon Production
+	}
+}
+
+function parseStatement(){
+	if(nextToken == "Print"){
+		parsePrint();
+	} else if (nextToken == "Char"){
+		parseAssign();
+	} else if (nextToken == "Type"){
+		parseVarDecl();
+	} else if (nextToken == "While"){
+		parseWhile();
+	} else if (nextToken == "If"){
+		parseIf();
+	} else if (nextToken == "LBRACE"){
+		parseBlock();
+	} else {
+		//error
+	}
+}
+
+function parsePrint(){
+	//match Print
+	//match LPAREN
+	parseExpr();
+	//match RPAREN
+}
+
+function parseAssign(){
+	parseId();
+	//match Assign
+	parseExpr();
+
+function parseVarDecl(){
+	//match Type
+	parseId();
+}
+
+function parseWhile(){
+	//match While
+	parseBooleanExpr();
+	parseBlock();
+}
+
+function parseIf(){
+	//match If
+	parseBooleanExpr();
+	parseBlock();
+}
+
+function parseExpr(){
+	if(nextToken == "Digit"){
+		parseIntExpr();
+	} else if(nextToken == "Quote"){
+		parseStringExpr();
+	} else if(nextToken == "LPAREN"){
+		parseBooleanExpr();
+	} else if(nextToken == "Char"){
+		parseId();
+	} else {
+		//error
+	}
+}
+
+function parseIntExpr(){
+	//match Digit
+	if(nextToken == "IntOp"){
+		//match IntOp
+		parseExpr();
+	}
+}
+
+function parseStringExpr(){
+	//match Quote
+	parseCharList();
+	//match Quote
+}
+
+function parseBooleanExpr(){
+	if(nextToken == "LPAREN"){
+		//match LPAREN
+		parseExpr();
+		//match BoolOp
+		parseExpr();
+		//match RPAREN
+	} else if(nextToken == "BoolVal"){
+		//match BoolVal
+	} else {
+		//error
+	}
+}
+
+function parseId(){
+	//match Char
+}
+
+function parseCharList(){
+	if(nextToken != "Quote"){
+		//match Char
+		parseCharList();
+	} else {
+		//Epsilon Production
+	}
+	
+}
